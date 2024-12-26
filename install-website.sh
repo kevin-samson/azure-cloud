2bf6d8fafd2cb9bc52d260d3f1840da4dcdacd7d
kevin <kevinsam2003@gmail.com>
2024-12-26 21:44:52 +0400

    install quixk

---
+++ install-website.sh
@@ -0,0 +1,8 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+source source ~/.bashrc
+nvm install node
+
+git clone https://github.com/Priyanshu9898/Expense-Tracker-App
+cd Expense-Tracker-App
+cd frontend
+npm install
