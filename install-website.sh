d3bf643be71d983cad9cae08a9c598973e23e136
kevin <kevinsam2003@gmail.com>
2024-12-26 21:51:29 +0400

    smakl change

--- install-website.sh
+++ install-website.sh
@@ -1,5 +1,5 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-source source ~/.bashrc
+source ~/.bashrc
 nvm install node
 
 git clone https://github.com/Priyanshu9898/Expense-Tracker-App
