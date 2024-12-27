25c62aee9dba40f521fcba499dc7461795a06649
kevin <kevinsam2003@gmail.com>
2024-12-27 10:56:22 +0400

    chanhes

--- website/backend/DB/Database.js
+++ website/backend/DB/Database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async (req, res) => {
-    const db = "mongodb://10.0.0.4:27017";
+    const db = "mongodb://10.0.0.4:27017/MainData";
 
     const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
 
